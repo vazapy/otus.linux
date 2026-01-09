8e653a3fc38803d1a259ee0f6ff0c1670c1fdcbb
vazapy <va.zapyatov@asi.ru>
2026-01-10 01:25:41 +0300

    2

--- vm-master/scripts/backup.sh
+++ vm-master/scripts/backup.sh
@@ -1,26 +1,26 @@
-#!binbash
+#!/bin/bash
 
 # Настройки
-CONTAINER_NAME=db-master
-DB_USER=root
-DB_PASS=root
-BACKUP_DIR=.backups
+CONTAINER_NAME="db-master"
+DB_USER="root"
+DB_PASS="root"
+BACKUP_DIR="./backups"
 DATE=$(date +%Y-%m-%d_%H-%M-%S)
-FILENAME=$BACKUP_DIRfull_backup_$DATE.sql
+FILENAME="$BACKUP_DIR/full_backup_$DATE.sql"
 
 # Создаем папку для бэкапов, если нет
 mkdir -p $BACKUP_DIR
 
-echo Start backup from $CONTAINER_NAME...
+echo "Start backup from $CONTAINER_NAME..."
 
 #
-# --all-databases  бэкапим всё
-# --master-data=1  добавляет команду CHANGE MASTER TO (с координатами!)
-# --single-transaction  чтобы не блокировать базу во время процесса
+# --all-databases : бэкапим всё
+# --master-data=1 : добавляет команду CHANGE MASTER TO (с координатами!)
+# --single-transaction : чтобы не блокировать базу во время процесса
 
-sudo docker exec $CONTAINER_NAME mysqldump -u$DB_USER -p$DB_PASS 
-  --all-databases 
-  --master-data=1 
-  --single-transaction  $FILENAME
+sudo docker exec $CONTAINER_NAME mysqldump -u$DB_USER -p$DB_PASS \
+  --all-databases \
+  --master-data=1 \
+  --single-transaction > $FILENAME
 
-echo Backup saved to $FILENAME
+echo "Backup saved to: $FILENAME"
--- vm-slave/scripts/restore.sh
+++ vm-slave/scripts/restore.sh
@@ -1,39 +1,39 @@
-#!binbash
+#!/bin/bash
 
 # Параметры подключения к Мастеру (они постоянные)
-MASTER_HOST=192.168.1.18
-REPL_USER=repl_user
-REPL_PASSWORD=slave_pass
+MASTER_HOST="192.168.1.18"
+REPL_USER="repl_user"
+REPL_PASSWORD="slave_pass"
 
 # Файл бэкапа (передаем первым аргументом при запуске скрипта)
-BACKUP_FILE=$1
+BACKUP_FILE="$1"
 
-if [ -z $BACKUP_FILE ]; then
-    echo Ошибка Укажите путь к файлу бэкапа!
-    echo Пример .restore.sh full_backup.sql
+if [ -z "$BACKUP_FILE" ]; then
+    echo "Ошибка: Укажите путь к файлу бэкапа!"
+    echo "Пример: ./restore.sh full_backup.sql"
     exit 1
 fi
 
-echo === Начинаем восстановление Слейва из файла $BACKUP_FILE ===
+echo "=== Начинаем восстановление Слейва из файла $BACKUP_FILE ==="
 
 # 1. Останавливаем Слейв (на всякий случай)
-echo Stopping Slave...
-sudo docker exec db-slave mysql -u root -proot -e STOP SLAVE;
+echo "Stopping Slave..."
+sudo docker exec db-slave mysql -u root -proot -e "STOP SLAVE;"
 
 # 2. Прописываем КУДА стучаться (Хост, Юзер, Пароль)
-# Координаты (FilePos) пока не трогаем, они прилетят из файла бэкапа!
-echo Configuring Master HostUser...
-sudo docker exec db-slave mysql -u root -proot -e CHANGE MASTER TO MASTER_HOST='$MASTER_HOST', MASTER_USER='$REPL_USER', MASTER_PASSWORD='$REPL_PASSWORD';
+# Координаты (File/Pos) пока не трогаем, они прилетят из файла бэкапа!
+echo "Configuring Master Host/User..."
+sudo docker exec db-slave mysql -u root -proot -e "CHANGE MASTER TO MASTER_HOST='$MASTER_HOST', MASTER_USER='$REPL_USER>
 
 # 3. Заливаем бэкап (Данные + Координаты)
-echo Importing Dump (Data + Coordinates)...
-# -i (интерактивно) тут не нужно, используем перенаправление ввода 
-sudo docker exec -i db-slave mysql -u root -proot  $BACKUP_FILE
+echo "Importing Dump (Data + Coordinates)..."
+# -i (интерактивно) тут не нужно, используем перенаправление ввода <
+sudo docker exec -i db-slave mysql -u root -proot < "$BACKUP_FILE"
 
 # 4. Запускаем Слейв
-echo Starting Slave...
-sudo docker exec db-slave mysql -u root -proot -e START SLAVE;
+echo "Starting Slave..."
+sudo docker exec db-slave mysql -u root -proot -e "START SLAVE;"
 
 # 5. Проверяем статус
-echo === STATUS ===
-sudo docker exec db-slave mysql -u root -proot -e SHOW SLAVE STATUS G  grep Running
+echo "=== STATUS ==="
+sudo docker exec db-slave mysql -u root -proot -e "SHOW SLAVE STATUS \G" | grep "Running:"
